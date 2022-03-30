e01f41109cff24371f2f3375529a309dd4a0a344
RANDRIAMIHANTA Fanilo <nfrandriamihanta@gmail.com>
2022-03-30 18:05:10 +0300

    refactoring

--- util/connect.js
+++ util/connect.js
@@ -6,14 +6,6 @@ exports.getClient = function getClient() {
     const uri = "mongodb+srv://nfrandria:dreamcomestrue@m1p9mean-fanilo.vaxze.mongodb.net/E-kaly?retryWrites=true&w=majority"
     const client = new MongoClient(uri)
     return client
-    // try {
-    //     await client.connect()
-    //     await listDatabases(client)
-    // } catch (e) {
-    //     console.error(e)
-    // } finally {
-    //     await client.close()
-    // }
 }
 
 exports.listDatabases = async function listDatabases(client) {
