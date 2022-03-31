60c5799c3c188544749aeb93b04dab2a3a8bf6a9
RANDRIAMIHANTA Fanilo <nfrandriamihanta@gmail.com>
2022-03-31 22:43:55 +0300

    resolve cors problem

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.use(cors({
     origin: ['http://localhost:4200', 'https://m1p9mean-fanilo.herokuapp.com/']
 }));
 
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+
 // define the first route
 router.get("/", function (req, res) {
     res.send("<h1>Server is working well!</h1>")
